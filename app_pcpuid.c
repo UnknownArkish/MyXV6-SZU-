7a2f99e734b09037049be862b34b537270b1d8d8
Arkish <645969958@qq.com>
2019-04-16 23:00:21 +0800

    .

--- app_pcpuid.c
+++ app_pcpuid.c
@@ -1,8 +1,8 @@
 // 此文件用以测试我们添加的getcpuid系统调用
 
+#include"types.h"
 #include"user.h"
 #include"stat.h"
-#include"types.h"
 
 int main(int argc, char** argv){
     printf(1, "My CPU id is %d\n", getcpuid());
